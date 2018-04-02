1afdd9da7efd0d037de36c5ce5a7477eef7157dc
Julia <jredston3@gatech.edu>
2018-04-02 18:31:08 -0400

    added future event list obj

--- futureEventList.py
+++ futureEventList.py
@@ -7,8 +7,37 @@ class futureEventList():
         self.ToastEvents = []
 
 
+
+    def pop(self):
+        min  = MeatEvents[0].ts
+        eventList = 'MEAT'
+        if(VegEvents[0].ts < min):
+            eventList = 'VEG'
+            min = VegEvents[0].ts
+        if(CheeseEvents[0].ts < min):
+            eventList = 'CHEESE'
+            min = CheeseEvents[0].ts
+        if(SauceEvents[0].ts < min):
+            eventList = 'SAUCE'
+            min = SauceEvents[0].ts
+        if(ToastEvents[0].ts < min):
+            eventList = 'TOAST'
+            min = ToastEvents[0].ts
+        
+        if eventList == 'MEAT':
+            return MeatEvents.pop(0)
+        elif eventList == 'VEG':
+             return VegEvents.pop(0)
+        elif eventList == 'CHEESE':
+             return CheeseEvents.pop(0)
+        elif eventList == 'SAUCE':
+             return SauceEvents.pop(0)
+        elif eventList == 'TOAST':
+             return ToastEvents.pop(0)
+
+
     def schedule_meat(self, event): #function to schedule event into priority queue (future event list)
-        i = 0
+        i =0
         while i < len(self.MeatEvents):
             if self.MeatEvents[i].ts > event.ts:
                 break
@@ -16,7 +45,7 @@ class futureEventList():
         self.MeatEvents.insert(i, event)
 
     def schedule_veg(self, event): #function to schedule event into priority queue (future event list)
-        i = 0
+        i =0
         while i < len(self.VegEvents):
             if self.VegEvents[i].ts > event.ts:
                 break
@@ -24,7 +53,7 @@ class futureEventList():
         self.VegEvents.insert(i, event)    
 
     def schedule_cheese(self, event): #function to schedule event into priority queue (future event list)
-        i = 0
+        i =0
         while i < len(self.CheeseEvents):
             if self.CheeseEvents[i].ts > event.ts:
                 break
@@ -32,7 +61,7 @@ class futureEventList():
         self.CheeseEvents.insert(i, event)
 
     def schedule_sauce(self, event): #function to schedule event into priority queue (future event list)
-        i = 0
+        i =0
         while i < len(self.SauceEvents):
             if self.SauceEvents[i].ts > event.ts:
                 break
@@ -40,7 +69,7 @@ class futureEventList():
         self.SauceEvents.insert(i, event)
 
     def schedule_toast(self, event): #function to schedule event into priority queue (future event list)
-        i = 0
+        i =0
         while i < len(self.ToastEvents):
             if self.ToastEvents[i].ts > event.ts:
                 break
